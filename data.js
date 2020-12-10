eea36cb03d37ad2d3abca188a376c6a2b6b7330c
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-12-10 11:46:46 -0700

    test2

--- data.js
+++ data.js
@@ -17,9 +17,9 @@ let q16 = {picture: 'q4.png', choices: ['c1', 'c2', 'c3', 'c4'], answer: 'c3'};
 let q17 = {picture: 'q5.png', choices: ['c1', 'c2', 'c3', 'c4'], answer: 'c4'};
 let q18 = {picture: 'q6.png', choices: ['c1', 'c2', 'c3', 'c4'], answer: 'c2'};
 
-let quiz1 = {name: 'quiz 1', id: 1, picture: 'q1.png', questions: [q1, q2, q3, q4, q5, q6]};
-let quiz2 = {name: 'quiz 2', id: 2, picture: 'q2.png', questions: [q7, q8, q9, q10, q11, q12]};
-let quiz3 = {name: 'quiz 3', id: 3, picture: 'q3.png', questions: [q13, q14, q15, q16, q17, q18]};
+let quiz1 = {name: 'quiz 1', id: 1, picture: 'CN.jpg', questions: [q1, q2, q3, q4, q5, q6]};
+let quiz2 = {name: 'quiz 2', id: 2, picture: 'ZOMBIE.jpg', questions: [q7, q8, q9, q10, q11, q12]};
+let quiz3 = {name: 'quiz 3', id: 3, picture: 'VIDEOG.png', questions: [q13, q14, q15, q16, q17, q18]};
 let quizzes = [quiz1, quiz2, quiz3];
 
 let scores = [];
